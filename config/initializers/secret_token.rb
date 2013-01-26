8e4ab5ed9709420c40a384af2539a31d2a2637f3
lingwu <agnovo_wl@hotmail.com>
2013-01-26 23:54:20 +0800

    on track is a program management application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ontrack::Application.config.secret_token = '164ed66607797043a1ba97d695c5cdac084765edc97b5bf3d2714956277a92eeacbad2d8d357dd88c9605e681c175ec14049f822b1e4ab96efcefb352c4781c3'
